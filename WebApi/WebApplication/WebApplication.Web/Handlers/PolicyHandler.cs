5dcadafe1002ac97f986ff9c4d635f61d8852247
s446683009 <446683009@qq.com>
2020-05-12 18:02:39 +0800

    测试jwt 在mvc 中的表现

---
+++ WebApi/WebApplication/WebApplication.Web/Handlers/PolicyHandler.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApplication.Web
+{
+    public class PolicyHandler : IAuthorizationHandler
+    {
+        public Task HandleAsync(AuthorizationHandlerContext context)
+        {
+            //从AuthorizationHandlerContext转成HttpContext，以便取出表求信息
+            var httpContext = (context.Resource as Microsoft.AspNetCore.Mvc.Filters.AuthorizationFilterContext).HttpContext;
+            //请求Url
+            var questUrl = httpContext.Request.Path.Value.ToUpperInvariant();
+            //是否经过验证
+            var isAuthenticated = httpContext.User.Identity.IsAuthenticated;
+            if (isAuthenticated)
+            {
+                //只有是登录了授权通过
+                context.Succeed(null);
+           }
+            else {
+              
+               ///var ath=httpContext.Request.Headers["Authorization"];
+               ///string token = ath.ToString().Substring("Bearer ".Length).Trim();
+               /////验证token
+               ///string payload =string.Empty;
+               ///string message = string.Empty;
+               ///var result=jwt.JWTHelper.ValidateJWT(token, "dkadladadadadaobngrgfgfgk",out payload,out message);
+                //只有是登录了授权通过
+                context.Fail();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebApi/WebApplication/WebApplication.Web/jwt/JWTHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+
+namespace WebApplication.Web.jwt
+{
+    public  static class JWTHelper
+    {
+        static IJwtAlgorithm algorithm = new HMACSHA256Algorithm();//HMACSHA256加密
+        static IJsonSerializer serializer = new JsonNetSerializer();//序列化和反序列
+        static IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();//Base64编解码
+        static IDateTimeProvider provider = new UtcDateTimeProvider();//UTC时间获取
+        public static string CreateJWT(Dictionary<string, object> payload,string secret)
+        {
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            return encoder.Encode(payload, secret);
+        }
+        public static bool ValidateJWT(string token, string secret,out string payload, out string message)
+        {
+            bool isValidted = false;
+            payload = "";
+            try
+            {
+
+                IJsonSerializer serializer = new JsonNetSerializer();
+                var provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+                payload = decoder.Decode(token, secret, verify: true);
+
+                isValidted = true;
+
+                message = "验证成功";
+            }
+            catch (TokenExpiredException)//当前时间大于负载过期时间（负荷中的exp），会引发Token过期异常
+            {
+                message = "过期了！";
+            }
+            catch (SignatureVerificationException)//如果签名不匹配，引发签名验证异常
+            {
+                message = "签名错误！";
+            }
+            return isValidted;
+        }
+    }
+}
